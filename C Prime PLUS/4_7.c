acaaa9100bdb39eacf0202a0ec689fb9427de91d
yeling <120004648+yelingy0@users.noreply.github.com>
2023-01-30 22:42:36 +0800

    Update 4_7.c

--- C Prime PLUS/4_7.c
+++ C Prime PLUS/4_7.c
@@ -6,7 +6,7 @@ int main(void)
     printf("*%d*\n",PAGES);
     printf("*%2d*\n",PAGES);//小于3，结果会自动调整为适合的字段宽度
     printf("*%10d*\n",PAGES);
-    printf("*%-10d*\n",PAGES);
+    printf("*%-10d*\n",PAGES);//-代表在左侧
 
     return 0;
 }
